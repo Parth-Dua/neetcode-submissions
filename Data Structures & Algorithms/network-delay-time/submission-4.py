8db2d9d6afda93f41e73a32de5ca3b5ada3becd8
Parth Dua <105743118+Parth-Dua@users.noreply.github.com>
2026-09-22 17:45:30 -0400

    Add: network-delay-time - submission-4

---
+++ Data Structures & Algorithms/network-delay-time/submission-4.py
@@ -0,0 +1,43 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+
+        graph = [[] for _ in range (n+1)]
+
+        # Adjacency list
+        for u,v,t in times: 
+            graph[u].append((v,t))
+
+        visited = set()
+
+        minheap = [(0,k)] # (dist from k, node)
+
+
+        dist = 0 
+        while len(visited) < n: 
+            
+            # something can't be visited
+            if not minheap: 
+                return -1
+
+            dist_from_k, node = heapq.heappop(minheap)
+
+            # If already visited do nothing
+            if node in visited:
+                continue
+
+            # This is unvisited node - greedily dist is finalized
+            visited.add(node)
+
+            dist = max(dist,dist_from_k)
+
+            # now populate the heap
+
+            for neighbor, weight in graph[node] :
+                # Add the unvisited ones to the minheap
+                
+                    heapq.heappush(minheap, (dist_from_k +weight ,neighbor))
+
+        return dist
+
+
+            
